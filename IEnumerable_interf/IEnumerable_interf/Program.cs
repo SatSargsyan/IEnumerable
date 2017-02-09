4e984c9e6b49a16dfb9f8dc2d42febaf6ec6263d
user <user@DESKTOP-I6SPLCP>
2017-02-09 18:38:26 +0400

    With yield return

---
+++ IEnumerable_interf/IEnumerable_interf/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace IEnumerable_interf
+{
+    
+    class Program
+    {
+          static void Main(string[] args)
+        {
+            foreach (var item in Fib().Skip(0).Take(20))
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+        static  IEnumerable<int> Fib()
+        {
+            int current = 0;
+            int next = 1;
+            while (true)
+            {
+                yield return current;
+                int temp = current;
+                current = next;
+                next = temp + current;
+            }
+        }
+    }
+}
